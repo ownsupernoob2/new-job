7e2759b2923f03fa87e3725cdf481afc48b04232
ownsupernoob2 <46392586+ownsupernoob2@users.noreply.github.com>
2019-05-30 10:02:45 +0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# new-job
-Created with CodeSandbox
---
+++ ignore/index.js
@@ -0,0 +1,5 @@
+import "sanitize.css";
+import "../style.scss";
+import "../style-about.scss";
+import "../style-contact.scss";
+import "../burger.scss";
